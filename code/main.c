507b3bebf69219006e1a5461790b0952b4daa150
Wu-Cheng-Yu <b09902024@ntu.edu.tw>
2021-04-04 17:58:13 +0800

    123

--- code/main.c
+++ code/main.c
@@ -1,18 +1,14 @@
 //reference:https://www.cnblogs.com/grandyang/p/8873471.html
-
 #include<stdio.h>
 #include<stdlib.h>
 #include<ctype.h>
 #include<string.h>
-#define MAX 10^6
+#define MAX 10^6 
 
 
 double eval(char*);
 double getnum(char* str, int* index);
 int isoperator(int);
-double eval_substring(char*,int);
-
-
 
 int main(void){
 	double result;
@@ -27,19 +23,28 @@ double eval(char* s){
 	int n = strlen(s);
 	double temp = 0,current=0,result=0;
 	char op = '+';
-	for(int i = 0;i<n;i++){
+	int j,count;
+	for(int i = 0; i < n; i++){
 		char c = s[i];
+		
 		if(isdigit(c)){
 			temp = temp*10+c-'0';
-		}else if(c=='('){
-			int j = i, count = 0;
+		}
+		
+		if(c=='('){
+			j = i; count = 0;
 			for(;i<n;i++){
 				if(s[i]=='(')count++;
 				if(s[i]==')')count--;
 				if(count==0)break;
 			}
-			temp = eval_substring(&s[j+1],i-j);
+			int length = i-j-1,position = j+1; 
+			char substr[length+1];
+			strncpy(substr,s+position,length);
+			substr[length] = '\0'; 
+			temp = eval(substr); 
 		}
+		
 		if(isoperator(c)||i== n-1){
 			switch(op){
 				case'+':current+=temp;break;
@@ -48,7 +53,7 @@ double eval(char* s){
 				case'/':current/=temp;break;
 			}
 			if(c=='+'||c=='-'||i==n-1){
-				result+=current;
+				result += current;
 				current =0;
 			}
 			op = c;
@@ -63,15 +68,8 @@ int isoperator(int c){
 	return (c=='+'||c=='-'||c=='*'||c=='/');
 }
 
-double eval_substring(char* string,int length){
-	if(length == 0){
-		return 0.0;
-	}
-	char str[length];
-	for(int index = 0;index<length;index++){
-		str[index] = string[index];
-	}
-	return eval(str);
-} 
+
+
+
 
 
