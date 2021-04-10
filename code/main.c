57e9dee0b8dd38837f578bc56cb24bff4b05e579
Wu-Cheng-Yu <b09902024@ntu.edu.tw>
2021-04-11 01:17:22 +0800

    j

--- code/main.c
+++ code/main.c
@@ -1,75 +1,135 @@
-//reference:https://www.cnblogs.com/grandyang/p/8873471.html
 #include<stdio.h>
 #include<stdlib.h>
-#include<ctype.h>
 #include<string.h>
-#define MAX 10^6 
+
+typedef struct node{
+	int data;
+	struct node* next;
+	struct node* prev; 
+}Node;
+ 
+typedef struct{
+	Node* head; 
+	Node* tail;
+}DDL;
+
+void leave(DDL* list){
+	Node* temp = (*list).tail;
+	if((*list).tail == NULL){return;}
+	else if((*list).tail == (*list).head){
+		(*list).tail = NULL;
+		(*list).head = NULL;
+		free(temp);
+		return;
+	}
+	else{
+		Node* temp = (*list).tail;
+		(*list).tail = ((*list).tail)->prev;
+		((*list).tail)->next = NULL;
+		free(temp);
+	}
+	return;
+}	 
 
 
-double eval(char*);
-double getnum(char* str, int* index);
-int isoperator(int);
+void enter(DDL* list, int label){
+	Node* new_node = (Node*)malloc(sizeof(Node));
+	new_node->data = label;
+	new_node->next = NULL;
+	
+	if((*list).head == NULL){
+		(*list).head = new_node;
+		(*list).tail = new_node;
+	}
+	else{
+	((*list).tail)->next = new_node;
+	new_node -> prev = (*list).tail;
+	((*list).tail) = new_node;
+	}
+	return;
+}
+
+void printll(DDL* list){
+	if((*list).head == NULL){
+		printf("%c",'\n');
+		return; 
+	}
+	
+	Node* node = (*list).head;
+	
+	if(node->prev == NULL && node->next==NULL){
+		printf("%d",node->data);
+		return;
+	} 
+	
+	Node* temp = NULL;
+	
+	while(node!=NULL){
+		printf("%d ",node->data);
+		if(node->next == temp){
+			node = node->prev;
+			temp = node;
+		}
+		else if(node->prev == temp){
+			node = node->next;
+			temp = node;
+		}
+			
+	}
+	printf("%c",'\n');
+	return;
+}
+
+void migrate(DDL* railone,DDL* railtwo){
+	if((*railtwo).head == NULL){
+		return;
+	}
+	
+	if ((*railone).head == NULL){
+		(*railone).head = (*railtwo).tail;
+		(*railone).tail = (*railtwo).head;
+		(*railtwo).head = NULL;
+		(*railtwo).tail = NULL;
+	} 
+	((*railone).tail)->next = ((*railtwo).tail);
+	(*railone).tail = (*railtwo).head;
+	(*railtwo).head = NULL;
+	(*railtwo).tail = NULL; 
+	return;
+}
 
 int main(void){
-	double result;
-	char string[MAX];
-	while(scanf("%s",string)!=EOF){
-		printf("%.15lf\n",eval(string));
-	} 
-	return 0;
-}
-
-double eval(char* s){
-	int n = strlen(s);
-	double temp = 0,current=0,result=0;
-	char op = '+';
-	int j,count;
-	for(int i = 0; i < n; i++){
-		char c = s[i];
-		
-		if(isdigit(c)){
-			temp = temp*10+c-'0';
-		}
-		
-		if(c=='('){
-			j = i; count = 0;
-			for(;i<n;i++){
-				if(s[i]=='(')count++;
-				if(s[i]==')')count--;
-				if(count==0)break;
-			}
-			int length = i-j-1,position = j+1; 
-			char substr[length+1];
-			strncpy(substr,s+position,length);
-			substr[length] = '\0'; 
-			temp = eval(substr); 
-		}
-		
-		if(isoperator(c)||i== n-1){
-			switch(op){
-				case'+':current+=temp;break;
-				case'-':current-=temp;break;
-				case'*':current*=temp;break;
-				case'/':current/=temp;break;
-			}
-			if(c=='+'||c=='-'||i==n-1){
-				result += current;
-				current =0;
-			}
-			op = c;
-			temp = 0;
-		}
+	int k,n,r,l,r_a,r_b;
+	scanf("%d%d",&k,&n);
+	DDL ddl[k]; 
+	char string[10];
+	
+	for(int i=0;i<k;i++){
+		ddl[i].head = NULL;
+		ddl[i].tail = NULL;
 	}
-	return result;
+	
+	for(int i=0;i<n;i++){
+		scanf("%s",string);
+			
+		if(!strcmp(string,"enter")){
+		scanf("%d %d",&r,&l);
+		enter(&ddl[r],l);
+		}
+		
+		if(!strcmp(string,"leave")){
+		scanf("%d",&r);
+		leave(&ddl[r]);
+		}
+		
+		if(!strcmp(string,"migrate")){
+		scanf("%d %d",&r_a,&r_b);
+		migrate(&ddl[r_a],&ddl[r_b]); 
+		} 
+	} 
+	 
+	for(int i=0;i<k;i++){
+		printll(&ddl[i]);
+	}
+	return 0;	
 }
-
-
-int isoperator(int c){
-	return (c=='+'||c=='-'||c=='*'||c=='/');
-}
-
-
-
-
-
-
